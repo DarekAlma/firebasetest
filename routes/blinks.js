f8d0aab0b5b07661dd38d91937023ed0565ecfa5
DarekAlma <darekaljuri@gmail.com>
2024-05-13 21:40:00 -0500

    comm5

--- routes/blinks.js
+++ routes/blinks.js
@@ -6,21 +6,42 @@ const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
+// Ruta para obtener todos los blinks de todos los usuarios
 router.get("/", async (req, res) => {
     try {
         const db = admin.firestore();
-        const usersSnapshot = await db.collection('users').get();
         const allBlinks = [];
+
+        // Obtener todos los usuarios
+        const usersSnapshot = await db.collection('users').get();
+
+        // Recorrer cada usuario
         usersSnapshot.forEach(userDoc => {
-            const userBlinks = userDoc.data().blinks;
-            allBlinks.push(...userBlinks);
+            // Obtener la colección de blinks del usuario actual
+            const blinksRef = userDoc.ref.collection('blinks');
+            
+            // Obtener todos los blinks del usuario actual
+            blinksRef.get().then(blinksSnapshot => {
+                // Agregar cada blink al arreglo de todos los blinks
+                blinksSnapshot.forEach(blinkDoc => {
+                    allBlinks.push(blinkDoc.data());
+                });
+
+                // Enviar todos los blinks como respuesta cuando se completen todas las promesas
+                if (allBlinks.length === usersSnapshot.size) {
+                    res.json(allBlinks);
+                }
+            }).catch(error => {
+                console.error('Error al obtener blinks del usuario:', error);
+                res.status(500).json({ message: 'Error interno del servidor' });
+            });
         });
-        res.json(allBlinks);
     } catch (error) {
-        console.error(error);
+        console.error('Error al obtener usuarios:', error);
         res.status(500).json({ message: 'Error interno del servidor' });
     }
 });
+
 
 router.post("/", async (req, res) => {
     try {
